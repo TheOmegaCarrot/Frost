2d6ec55e9360a9a210a1a0f6edfb5473afbaef25
TheOmegaCarrot <theomegacarrot@gmail.com>
2026-01-10 15:13:50 -0500

    to_int and to_float builtins

--- implementation/cpp/functions/builtins/type-conversions.cpp
+++ implementation/cpp/functions/builtins/type-conversions.cpp
@@ -1,5 +1,7 @@
 #include "builtins-common.hpp"
 
+#include <frost/builtin.hpp>
+
 namespace frst
 {
 Value_Ptr to_string(builtin_args_t args)
@@ -7,8 +9,20 @@ Value_Ptr to_string(builtin_args_t args)
     return args.at(0)->to_string();
 }
 
+Value_Ptr to_int(builtin_args_t args)
+{
+    return args.at(0)->to_int();
+}
+
+Value_Ptr to_float(builtin_args_t args)
+{
+    return args.at(0)->to_float();
+}
+
 void inject_type_conversions(Symbol_Table& table)
 {
     INJECT(to_string, 1, 1);
+    INJECT(to_int, 1, 1);
+    INJECT(to_float, 1, 1);
 }
 } // namespace frst
