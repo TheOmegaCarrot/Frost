82626c5f92ea2e3b2090f7c386951aa7bf9ff3d5
TheOmegaCarrot <theomegacarrot@gmail.com>
2026-01-24 17:59:44 -0500

    or_else

--- implementation/cpp/functions/builtins/and_then.cpp
+++ implementation/cpp/functions/builtins/and_then.cpp
@@ -16,8 +16,19 @@ BUILTIN(and_then)
     return GET(1, Function)->call({args.at(0)});
 }
 
+BUILTIN(or_else)
+{
+    REQUIRE_ARGS("or_else", ANY, TYPES(Function));
+
+    if (args.at(0)->is<Null>())
+        return GET(1, Function)->call({});
+
+    return args.at(0);
+}
+
 void inject_and_then(Symbol_Table& table)
 {
     INJECT(and_then, 2, 2);
+    INJECT(or_else, 2, 2);
 }
 } // namespace frst
