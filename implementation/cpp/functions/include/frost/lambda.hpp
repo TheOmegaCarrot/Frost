657d0aced42f489e19d8aa957fe063b7aafe273f
TheOmegaCarrot <theomegacarrot@gmail.com>
2026-01-26 22:45:46 -0500

    move Lambda logic to cpp file

--- implementation/cpp/functions/include/frost/lambda.hpp
+++ implementation/cpp/functions/include/frost/lambda.hpp
@@ -12,25 +12,6 @@
 
 namespace frst::ast
 {
-namespace
-{
-
-std::generator<Statement::Symbol_Action> node_to_sym_seq(
-    const frst::ast::Statement::Ptr& node)
-{
-    return node->symbol_sequence();
-}
-
-Value_Ptr promote_if_weak(const Value_Ptr& fn)
-{
-    if (auto weak_closure =
-            std::dynamic_pointer_cast<Weak_Closure>(fn->raw_get<Function>()))
-        return Value::create(weak_closure->promote());
-    else
-        return fn;
-}
-
-} // namespace
 
 class Lambda final : public Expression
 {
@@ -45,132 +26,14 @@ class Lambda final : public Expression
     ~Lambda() final = default;
 
     Lambda(std::vector<std::string> params, std::vector<Statement::Ptr> body,
-           std::optional<std::string> vararg_param = {})
-        : params_{std::move(params)}
-        , body_{std::make_shared<std::vector<Statement::Ptr>>(std::move(body))}
-        , vararg_param_{std::move(vararg_param)}
-    {
-        std::flat_set<std::string> param_set{std::from_range, params_};
+           std::optional<std::string> vararg_param = {});
 
-        if (vararg_param_)
-            param_set.insert(vararg_param_.value());
+    [[nodiscard]] Value_Ptr evaluate(const Symbol_Table& syms) const final;
 
-        if (const auto expected_param_set_size =
-                params_.size() + (vararg_param_.has_value() ? 1 : 0);
-            expected_param_set_size != param_set.size())
-        {
-            throw Frost_Unrecoverable_Error{"Closure has duplicate parameters"};
-        }
-
-        std::flat_set<std::string> names_defined_so_far{std::from_range,
-                                                        param_set};
-
-        for (const Statement::Symbol_Action& name :
-             *body_
-                 | std::views::transform(&node_to_sym_seq)
-                 | std::views::join)
-        {
-            name.visit(Overload{
-                [&](const Statement::Definition& defn) {
-                    if (param_set.contains(defn.name))
-                    {
-                        throw Frost_Unrecoverable_Error{
-                            fmt::format("Closure local definition cannot "
-                                        "shadow parameter: {}",
-                                        defn.name)};
-                    }
-
-                    names_defined_so_far.insert(defn.name);
-                },
-                [&](const Statement::Usage& used) {
-                    if (used.name
-                        != "self"
-                        && !names_defined_so_far.contains(used.name))
-                    {
-                        names_to_capture_.insert(used.name);
-                    }
-                },
-            });
-        }
-    }
-
-    [[nodiscard]] Value_Ptr evaluate(const Symbol_Table& syms) const final
-    {
-        Symbol_Table captures;
-        for (const std::string& name : names_to_capture_)
-        {
-            if (not syms.has(name))
-            {
-                throw Frost_Unrecoverable_Error{fmt::format(
-                    "No definition found for captured symbol: {}", name)};
-            }
-
-            const auto& value = syms.lookup(name);
-
-            if (value->is<Function>())
-            {
-                // special handling to promote any closure self-reference to a
-                // strong reference
-                captures.define(name, promote_if_weak(value));
-            }
-            else
-            {
-                captures.define(name, value);
-            }
-        }
-
-        auto closure = std::make_shared<Closure>(
-            params_, body_, std::move(captures), vararg_param_);
-
-        auto weak_closure = Value::create(
-            Function{std::make_shared<Weak_Closure>(std::weak_ptr{closure})});
-
-        closure->inject_capture("self", weak_closure);
-
-        return Value::create(Function{std::move(closure)});
-    }
-
-    std::generator<Symbol_Action> symbol_sequence() const final
-    {
-        const auto get_name = [](const auto& action) {
-            return action.name;
-        };
-
-        std::flat_set<std::string> defns{std::from_range, params_};
-
-        if (vararg_param_)
-            defns.insert(vararg_param_.value());
-
-        for (const Statement::Symbol_Action& action :
-             *body_
-                 | std::views::transform(&node_to_sym_seq)
-                 | std::views::join)
-        {
-            const auto name = action.visit(get_name);
-            if (std::holds_alternative<Statement::Definition>(action))
-                defns.insert(name);
-            else if (not defns.contains(name))
-                co_yield action;
-        }
-    }
+    std::generator<Symbol_Action> symbol_sequence() const final;
 
   protected:
-    std::string node_label() const final
-    {
-        if (vararg_param_ && params_.size() != 0)
-        {
-            return fmt::format("Lambda({}, ...{})", fmt::join(params_, ", "),
-                               vararg_param_.value());
-        }
-        else if (vararg_param_ && params_.size() == 0)
-        {
-            return fmt::format("Lambda(...{})", vararg_param_.value());
-        }
-        else
-        {
-            return fmt::format("Lambda({})", fmt::join(params_, ", "));
-        }
-    }
+    std::string node_label() const final;
 
     std::generator<Child_Info> children() const final
     {
