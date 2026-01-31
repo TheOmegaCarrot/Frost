c0632a4e7f3a4d664f15428245f373a10ca1d2ac
TheOmegaCarrot <theomegacarrot@gmail.com>
2026-01-30 22:21:04 -0500

    reorganize builtin injection - expose each component

--- implementation/cpp/functions/builtins.cpp
+++ implementation/cpp/functions/builtins.cpp
@@ -4,37 +4,4 @@
 namespace frst
 {
 
-#define X_INJECT                                                               \
-    X(structure_ops)                                                           \
-    X(type_checks)                                                             \
-    X(type_conversions)                                                        \
-    X(pack_call)                                                               \
-    X(debug_helpers)                                                           \
-    X(error_handling)                                                          \
-    X(output)                                                                  \
-    X(free_operators)                                                          \
-    X(math)                                                                    \
-    X(string_ops)                                                              \
-    X(regex)                                                                   \
-    X(mutable_cell)                                                            \
-    X(ranges)                                                                  \
-    X(and_then)                                                                \
-    X(os)                                                                      \
-    X(streams)                                                                 \
-    X(json)
-
-#define X(F) void inject_##F(Symbol_Table&);
-
-X_INJECT
-
-#undef X
-
-void inject_builtins(Symbol_Table& table)
-{
-#define X(F) inject_##F(table);
-
-    X_INJECT
-
-#undef X
-}
 } // namespace frst
