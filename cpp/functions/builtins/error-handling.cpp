206f6be85abd4f4e1777465cb6ea8c054db87127
Ethan Hancock <ethanhancock@me.com>
2026-03-16 20:22:15 -0400

    re-simplify try_call

--- cpp/functions/builtins/error-handling.cpp
+++ cpp/functions/builtins/error-handling.cpp
@@ -8,51 +8,47 @@ using namespace std::literals;
 namespace frst
 {
 
+BUILTIN(try_call)
+{
+    // clang-format off
+    REQUIRE_ARGS("try_call",
+            PARAM("function", TYPES(Function)),
+            PARAM("args", TYPES(Array)));
+    // clang-format on
+
+    STRINGS(ok, value, error, trace);
+
+    try
+    {
+        return Value::create(
+            Value::trusted,
+            Map{{strings.value, GET(0, Function)->call(GET(1, Array))},
+                {strings.ok, Value::create(true)}});
+    }
+    catch (Frost_Recoverable_Error& err)
+    {
+        Map result_map{
+            {strings.ok, Value::create(false)},
+            {strings.error, Value::create(String{err.what()})}};
+
+        auto bt = err.take_backtrace();
+        if (!bt.empty())
+        {
+            Array frames;
+            frames.reserve(bt.size());
+            for (auto& frame : bt)
+                frames.push_back(Value::create(std::move(frame)));
+            result_map.emplace(strings.trace,
+                               Value::create(std::move(frames)));
+        }
+
+        return Value::create(Value::trusted, std::move(result_map));
+    }
+}
+
 void inject_error_handling(Symbol_Table& table)
 {
-    table.define(
-        "try_call",
-        Value::create(Function{std::make_shared<Builtin>(
-            [](builtin_args_t args) -> Value_Ptr {
-                // clang-format off
-                REQUIRE_ARGS("try_call",
-                        PARAM("function", TYPES(Function)),
-                        PARAM("args", TYPES(Array)));
-                // clang-format on
-
-                STRINGS(ok, value, error, trace);
-
-                try
-                {
-                    return Value::create(
-                        Value::trusted,
-                        Map{{strings.value,
-                             GET(0, Function)->call(GET(1, Array))},
-                            {strings.ok, Value::create(true)}});
-                }
-                catch (Frost_Recoverable_Error& err)
-                {
-                    Map result_map{
-                        {strings.ok, Value::create(false)},
-                        {strings.error, Value::create(String{err.what()})}};
-
-                    auto bt = err.take_backtrace();
-                    if (!bt.empty())
-                    {
-                        Array frames;
-                        frames.reserve(bt.size());
-                        for (auto& frame : bt)
-                            frames.push_back(
-                                Value::create(std::move(frame)));
-                        result_map.emplace(strings.trace,
-                                           Value::create(std::move(frames)));
-                    }
-
-                    return Value::create(Value::trusted,
-                                         std::move(result_map));
-                }
-            },
-            "try_call", Builtin::Arity{.min = 2, .max = 2})}));
+    INJECT(try_call, 2);
 }
 
 } // namespace frst
