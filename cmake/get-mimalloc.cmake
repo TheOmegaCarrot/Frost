730df887dfc9e45f991bfed00e27ee4c04dc055b
Ethan Hancock <ethanhancock@me.com>
2026-04-23 20:37:41 -0400

    switch to using mimalloc

---
+++ cmake/get-mimalloc.cmake
@@ -0,0 +1,21 @@
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(FROST_MI_VALGRIND OFF)
+else()
+    find_program(VALGRIND_EXECUTABLE valgrind)
+    if(VALGRIND_EXECUTABLE)
+        set(FROST_MI_VALGRIND ON)
+    else()
+        set(FROST_MI_VALGRIND OFF)
+    endif()
+endif()
+
+CPMAddPackage(
+    NAME mimalloc
+    GITHUB_REPOSITORY microsoft/mimalloc
+    GIT_TAG v2.2.4
+    OPTIONS
+        "MI_BUILD_SHARED OFF"
+        "MI_BUILD_TESTS OFF"
+        "MI_BUILD_OBJECT OFF"
+        "MI_VALGRIND ${FROST_MI_VALGRIND}"
+)
