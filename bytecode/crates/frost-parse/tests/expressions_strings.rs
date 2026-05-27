e356d527270acca609f5a5b218a38e7d42d0f5e0
Ethan Hancock <ethanhancock@me.com>
2026-05-26 20:55:32 -0400

    more tests

--- bytecode/crates/frost-parse/tests/expressions_strings.rs
+++ bytecode/crates/frost-parse/tests/expressions_strings.rs
@@ -220,6 +220,77 @@ fn error_multiline_closing_not_own_line() {
     assert!(err.contains("own line"), "error was: {err}");
 }
 
+// -- Additional escape edge cases --
+
+#[test]
+fn escape_hex_null() {
+    let expr = parse_expr(r"'\x00'");
+    assert_eq!(str_bytes(&expr), &[0x00]);
+}
+
+#[test]
+fn error_hex_invalid_digits() {
+    let err = parse_err(r"'\xZZ'");
+    assert!(err.contains("hex escape"), "error was: {err}");
+}
+
+#[test]
+fn error_dollar_escape_in_regular_string() {
+    // \$ is only valid in format strings, not regular strings
+    let err = parse_err(r"'literal \$ here'");
+    assert!(err.contains("invalid escape"), "error was: {err}");
+}
+
+// -- Simple strings with embedded quotes --
+
+#[test]
+fn single_quote_with_double_quotes() {
+    let expr = parse_expr(r#"'has "double" quotes'"#);
+    assert_eq!(str_bytes(&expr), b"has \"double\" quotes");
+}
+
+#[test]
+fn double_quote_with_single_quotes() {
+    let expr = parse_expr(r#""has 'single' quotes""#);
+    assert_eq!(str_bytes(&expr), b"has 'single' quotes");
+}
+
+// -- Raw string edge cases --
+
+#[test]
+fn raw_string_with_opposite_quotes() {
+    let expr = parse_expr(r#"R'(has "quotes" inside)'"#);
+    assert_eq!(str_bytes(&expr), br#"has "quotes" inside"#);
+}
+
+#[test]
+fn raw_string_with_backslash_sequences() {
+    let expr = parse_expr(r"R'(\n\t\r\0\xff)'");
+    assert_eq!(str_bytes(&expr), br"\n\t\r\0\xff");
+}
+
+// -- Multiline single-quote form --
+
+#[test]
+fn multiline_single_quote() {
+    let expr = parse_expr("'''\n    hello\n    '''");
+    assert_eq!(str_bytes(&expr), b"hello");
+}
+
+#[test]
+fn multiline_single_quote_with_double_quotes() {
+    let expr = parse_expr("'''\n    has \"quotes\"\n    '''");
+    assert_eq!(str_bytes(&expr), b"has \"quotes\"");
+}
+
+// -- Multiline with only empty lines --
+
+#[test]
+fn multiline_only_empty_lines() {
+    let expr = parse_expr("\"\"\"\n\n\n\"\"\"");
+    assert_eq!(str_bytes(&expr), b"\n");
+}
+
 // -- Strings in expressions --
 
 #[test]
