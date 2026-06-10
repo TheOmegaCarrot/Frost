91dc68ffd8621c102126dd050802fba2778db9df
Ethan Hancock <ethanhancock@me.com>
2026-06-09 22:57:36 -0400

    first few partial vm tests

--- bytecode/crates/frost-runtime/src/lib.rs
+++ bytecode/crates/frost-runtime/src/lib.rs
@@ -5,3 +5,5 @@ pub use core::{
     FrostArray, FrostError, FrostFloat, FrostMap, FrostResult, KEYWORDS, MapKey, NativeFunction,
     Value, from_value, to_value,
 };
+
+pub use vm::{Bytecode, CompiledFunction, NameTableEntry, ProgramResult, Vm};
